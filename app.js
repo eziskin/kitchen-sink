2cb9c15c5b3bc4c7f4c15049527070d4c580ae55
eziskin <eliziskin@gmail.com>
2019-04-25 07:53:10 -0700

    kitchen sink lab

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var val = subtract(add(2,3), subtract(5, 3));
+
+console.log(val);
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
